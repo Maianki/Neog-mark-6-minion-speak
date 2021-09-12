3a577450e3c5dbcdd58f8944d5e959cce98321c7
Ankit Kumain <kumankit10@gmail.com>
2021-09-12 12:12:28 +0530

    feat: added feature to translate english to minion language

---
+++ main.js
@@ -0,0 +1,33 @@
+var txtInput = document.querySelector("#txt-input")
+var btnTranslate = document.querySelector(".btn-translate")
+var outputContainer = document.querySelector("#output-container")
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+//function to generate translationURL using API 
+function translationURL(userInput){
+    return serverURL+"?text="+userInput
+}
+
+//function to handle error
+function errorHandler(error){
+    console.log(error)
+    alert("Something went wrong(You might have exhausted your 5 translation)! Try again after some time")
+}
+
+//Function to translate userInput to banana language
+function translateToBanana(){
+    //store userInput
+    var userInput = txtInput.value;
+    //fetch the url and output the translated text in output container
+    fetch(translationURL(userInput))
+    .then((response)=>response.json())
+    .then((json)=>{
+        var translatedOutput=json.contents.translated;
+        console.log(translatedOutput)
+        outputContainer.innerHTML = translatedOutput
+    })
+    .catch(errorHandler)
+}
+//Calling addEventListener
+btnTranslate.addEventListener("click",translateToBanana)
